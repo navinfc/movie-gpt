0f0640288a59ada0c3eb1e10d439dd01925466b6
Navin Chandrakar <88479575+navinfc@users.noreply.github.com>
2024-02-22 17:13:01 +0530

    change avatar logo

--- src/utils/constant.js
+++ src/utils/constant.js
@@ -1,3 +1,3 @@
 export const LOGO = "https://static.vecteezy.com/system/resources/previews/019/956/198/original/netflix-transparent-netflix-free-free-png.png";
 
-export const USERAVATAR = "https://avatars.githubusercontent.com/u/88479575?v=4"
+export const USERAVATAR = "https://static.vecteezy.com/system/resources/previews/026/785/285/original/inter-miami-fc-logo-on-transparent-background-free-vector.jpg";

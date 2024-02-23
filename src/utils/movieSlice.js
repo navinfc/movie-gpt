83c02b5c5985f1ddfa9f9db584b02496c432ea31
Navin Chandrakar <88479575+navinfc@users.noreply.github.com>
2024-02-24 00:00:29 +0530

    added new state in movieSlice

--- src/utils/movieSlice.js
+++ src/utils/movieSlice.js
@@ -4,11 +4,15 @@ const movieSlice = createSlice({
     name: "movies",
     initialState: {
         nowPlayingMovies: null,
+        trailerVideo: null,
     },
     reducers: {
         addNowPlayingMovies: (state, action) => {
             state.nowPlayingMovies = action.payload;
         },
+        addTrailerVideo: (state, action) => {
+            state.trailerVideo = action.payload;
+        }
     }
 })
 

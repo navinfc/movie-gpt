b0c0f8252f67b114faa607ca98b5291cc52317b2
Navin Chandrakar <88479575+navinfc@users.noreply.github.com>
2024-02-26 18:06:20 +0530

    adding classname attribute to form

--- src/components/GptSearchBar.js
+++ src/components/GptSearchBar.js
@@ -3,7 +3,7 @@ import React from 'react'
 const GptSearchBar = () => {
   return (
     <div>
-      <form>
+      <form className='p-6 m-6'>
         <input type="text" className='' placeholder="What would you like to watch today?"/>
         <button className=''>Search Button</button>
       </form>

2dd1757ab3538fb61fa2763d6c8e30993bfa93c9
Navin Chandrakar <88479575+navinfc@users.noreply.github.com>
2024-02-26 18:03:30 +0530

    Created gpt search and suggestions components

---
+++ src/components/GptSearchBar.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const GptSearchBar = () => {
+  return (
+    <div>
+      <form>
+        <input type="text" className='' placeholder="What would you like to watch today?"/>
+        <button className=''>Search Button</button>
+      </form>
+    </div>
+  )
+}
+
+export default GptSearchBar
--- src/components/Header.js
+++ src/components/Header.js
@@ -5,6 +5,7 @@ import { useNavigate } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
 import { addUser, removeUser } from '../utils/userSlice';
 import { LOGO } from '../utils/constant';
+import { toggleGptSearchView } from '../utils/gptSlice';
 
 const Header = () => {
 
@@ -17,6 +18,11 @@ const Header = () => {
     }).catch((error) => {
       navigate("/error");
     });
+  }
+
+  const handleGptSearchClick = () => {
+    // Toggle GPT search button
+    dispatch(toggleGptSearchView());
   }
 
   useEffect(() => {
@@ -37,10 +43,12 @@ const Header = () => {
   }, []);
 
   return (
-    <div className='absolute w-screen px-8 py-2 bg-gradient-to-b from-black z-10 flex flex-col md:flex-row justify-between'>
+    <div className='absolute w-screen px-8 py-2 bg-gradient-to-b from-black z-10 flex justify-between'>
       <img className='w-44 mx-auto md:mx-0 bg-none' src={LOGO} alt="logo" />
       {
-        user && (<div className='flex'>
+        user && (
+        <div className='flex p-2'>
+          <button className='m-2 px-4 bg-cyan-600 text-white h-10 my-auto rounded-lg' onClick={handleGptSearchClick}>GPT Search</button>
           <img  className="w-12 h-12 m-auto mx-1" src={user?.photoURL} alt="User Logo"/>
           <button className='text-2xl font-bold text-red-500' onClick={handleSignOut}>Sign Out</button>
         </div>)
